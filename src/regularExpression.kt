4e9d848a2997c6ceeb56d38afe2ecfb41e7c8bae
DJ Harrison <fuegoisfire@gmail.com>
2026-04-10 09:17:17 -0700

    Added header and todo items to regularExpression.kt

--- src/regularExpression.kt
+++ src/regularExpression.kt
@@ -1,10 +1,24 @@
+/************************************************************
+ * Name: DJ Harrison
+ * Date: 4/10/2026
+ * Assignment: Text Search Regular Expressions Assignment
+ * Class Number: 410
+ * Description: search through a data file using regular expressions
+ ************************************************************/
+
+//TODO: Write code to import the file
+//TODO: Write RegEx to search the file for two fields (Department & Class Number)
+//TODO: Search function should output the course title + all trailing lines until the next course
+
+//  Example file output:
 /*
+FOOT ITEM DEPT COURSE  CLUSTER ADMIN  COURSE          CR     INSTR     BLDG                  START  END   CLASS
+NOTE NUM_ _DIV_ NUM_SECT _ID_  UNIT___TITLE____ _CR_EQUIV ___NAME_____ NUM _ROOM ___DAYS___ _TIME_ _TIME_ _CAP_  _ENR_ OPEN_ FTES
+     0516 ABF   243  AN        CJ  ADV UNIBODY   6.0  6.0 ChristensenE 018 0155  F           7:30A  8:30A    20     20     0  8.0
+                                                          ChristensenE 018 0155  MTW         9:30A 10:30A
+                                                          ChristensenE 018 0155  DAILY       8:30A  9:30A
+*/
 
-(555) 222-2222
-(556) 333-1234
-(509) 312-2121
-
-
-
-
- */
+fun main() {
+    print("Hello World")
+}
